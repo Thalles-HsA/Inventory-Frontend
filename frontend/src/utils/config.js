3453022604593a54c971597708f5dd5f6b16835d
Thalles-HsA <thsa.henrique@gmail.com>
2023-04-06 08:29:07 -0300

    refactor(config): :recycle: refatora código do arquivo config.js
    Aprimora a estrutura do arquivo config.js, tornando o código mais legível e organizado. Remove duplicações e utiliza funções auxiliares para simplificar o código.
    
    See also: 6de96de

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,15 +1,9 @@
 export const api = "http://localhost:5000/api";
 
-export const requestConfig = (method, data, token = null, image = null) => {
+export const requestConfig = (method, data, token = null) => {
     let config;
   
-    if (image) {
-      config = {
-        method: method,
-        body: data,
-        headers: {},
-      };
-    } else if (method === "DELETE" || data === null) {
+    if (method === "DELETE" || data === null) {
       config = {
         method: method,
         headers: {},
